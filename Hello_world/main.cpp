380bdfbd68254fc92aa0ea0adeb6644632145b50
yahya <115286845+yahya-goorakani@users.noreply.github.com>
2022-12-06 12:40:02 +0000

    create_heloWorld

---
+++ Hello_world/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main (){
+
+	std::cout<<"_______________\n";
+
+	std::cout<<"\nHello world!\n";
+
+	std::cout<<"\nHello yahya!";
+
+	std::cout<<"\n_______________";
+
+	return 0;
+}
