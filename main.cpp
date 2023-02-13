c974690cdf7082c904a5a85d825686521a7dc5fb
yahya <115286845+yahya-goorakani@users.noreply.github.com>
2023-02-13 20:39:29 +0000

    .

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main(){
+
+cout<<"sqrt(36):"<<sqrt(36)<<endl;
+cout<<"abs(-19):"<<abs(-19)<<endl;
+cout<<"pow(5^3):"<<pow(5,3)<<endl;
+cout<<"ceil(12.12):"<<ceil(12.12)<<endl;
+cout<<"trunc(6.8):"<<trunc(6.8)<<endl;
+cout<<"floor(12.12):"<<floor(12.12)<<endl;
+cout<<"fmod(8%4):"<<fmod(8,4)<<endl;
+cout<<"log10(100):"<<log10(100)<<endl;
+cout<<"log2(32):"<<log2(32)<<endl;
+}
