0e29908bd79b824f329f3a5b9516920e04eee24d
yahya <115286845+yahya-goorakani@users.noreply.github.com>
2023-02-13 16:09:26 +0000

    ,

---  sizeـofـvariables/main.cpp
+++  sizeـofـvariables/main.cpp
@@ -6,9 +6,13 @@ int main(){
     float   b=12.3;
     char    c='y';
     bool    d=true;
+    string  j="yahya";
+
     
-    cout<<sizeof(a)<<endl;
-    cout<<sizeof(b)<<endl;
-    cout<<sizeof(c)<<endl;
-    cout<<sizeof(d)<<endl;
+ 
+    cout<<"size of integer:"<<sizeof(a)<<endl;
+    cout<<"The size of the decimal number:"<<sizeof(b)<<endl;
+    cout<<"font size:"<<sizeof(c)<<endl;
+    cout<<"size of boolean:"<<sizeof(d)<<endl;
+    cout<<"size of sring:"<<sizeof(j)<<endl;
 }
