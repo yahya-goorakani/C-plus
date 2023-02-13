ab3c7eb677a0083173600be28ebcbca022581d99
yahya <115286845+yahya-goorakani@users.noreply.github.com>
2023-02-13 15:48:28 +0000

    create project

---
+++  sizeـofـvariables/main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int     a=123;
+    float   b=12.3;
+    char    c='y';
+    bool    d=true;
+    
+    cout<<sizeof(a)<<endl;
+    cout<<sizeof(b)<<endl;
+    cout<<sizeof(c)<<endl;
+    cout<<sizeof(d)<<endl;
+}
