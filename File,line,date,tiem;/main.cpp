8b3b39b123e9d587052d4a1a8e8aca2a5d5ac555
yahya <115286845+yahya-goorakani@users.noreply.github.com>
2023-04-17 11:41:22 +0000

    .

---
+++ File,line,date,tiem;/main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+
+int main() {
+
+        std::cout<< __FILE__ <<std::endl;
+        std::cout<< __LINE__ <<std::endl;
+        std::cout<< __DATE__ <<std::endl;
+        std::cout<< __TIME__ <<std::endl;
+        return 0;
+}
